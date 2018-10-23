778c84f20d15267777133a56abdc953c7fc2bed5
Michał <michal.ch97@gmail.com>
2018-10-23 21:52:36 +0200

    First commit: gitignore and program template

---
+++ AdaptiveProgrammingModel/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaptiveProgrammingModel
+{
+    //TODO - change class name
+    //https://github.com/mpostol/TP/tree/master/TPA/Reflection/Model
+    public class Class1
+    {
+    }
+}
