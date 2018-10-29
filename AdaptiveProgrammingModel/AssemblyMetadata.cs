66ea2ec838344004beebb352cd4280022c7092a5
Michał <michal.ch97@gmail.com>
2018-10-29 20:43:25 +0100

    Added classes for assembly and namespace

---
+++ AdaptiveProgrammingModel/AssemblyMetadata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Xml.XPath;
+
+namespace AdaptiveProgrammingModel
+{
+    public class AssemblyMetadata
+    {
+        private string assemblyName;
+        private IEnumerable<NamespaceMetadata> namespaces;
+
+        public AssemblyMetadata(Assembly assembly)
+        {
+            assemblyName = assembly.ManifestModule.Name;
+            namespaces = from Type t in assembly.GetTypes()
+                         where t.GetVisible()
+                         group t by t.GetNamespace()
+                         into g
+                         orderby g.Key
+                         select new NamespaceMetadata(g.Key, g);
+        }
+    }
+}
---
+++ AdaptiveProgrammingModel/NamespaceMetadata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdaptiveProgrammingModel
+{
+    public class NamespaceMetadata
+    {
+        private string namespaceName;
+        private IEnumerable<TypeMetadata> types;
+
+        public NamespaceMetadata(string namespaceName, IEnumerable<Type> types)
+        {
+            this.namespaceName = namespaceName;
+            this.types = from type in types orderby type.Name select new TypeMetadata(type);
+        }
+    }
+}
